0150a5c49137613e9cfd0e408ea9783433231cc6
Dev <dutchmandynamics.kajmunk@gmail.com>
2026-02-16 20:42:39 +0100

    First commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Header from '../components/Header';
+
+const IndexPage = () => (
+  <main>
+    <Header />
+  </main>
+);
+
+export const Head = () => <title>CanSat Kajmunk 25/26</title>;
+
+export default IndexPage;
